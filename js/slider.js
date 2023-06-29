9d86972048f986562c4255f23cae42635c0e8dae
edzayaa <haswell252@gmail.com>
2023-06-29 02:11:58 -0400

    version 6
    version 6

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,38 @@
-$(".custom-carousel").owlCarousel({
-  autoWidth: true,
-  loop: true
-});
-$(document).ready(function () {
-  $(".custom-carousel .item").click(function () {
-    $(".custom-carousel .item").not($(this)).removeClass("active");
-    $(this).toggleClass("active");
-  });
-});
+(function ($) {
+
+	// Products Slick
+	$('.products-slick').each(function () {
+		var $this = $(this),
+			$nav = $this.attr('data-nav');
+
+		$this.slick({
+			slidesToShow: 4,
+			slidesToScroll: 2,
+			autoplay: true,
+			infinite: true,
+			speed: 300,
+			dots: true,
+			arrows: true,
+			appendArrows: $nav ? $nav : false,
+			responsive: [{
+				breakpoint: 991,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			},
+			]
+		});
+	});
+
+
+
+
+})(jQuery);
